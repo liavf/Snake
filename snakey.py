af0f228c936300b5dceab9411ea42f1f7e868d05
liavf <liav.feldman@gmail.com>
2022-05-26 10:50:44 +0300

    linked list

--- snakey.py
+++ snakey.py
@@ -4,14 +4,81 @@ UP = "Up"
 DOWN = "Down"
 MOVEKEYS = [LEFT, RIGHT, UP, DOWN]
 
+class Node:
+    def __init__(self, location=None, next=None, prev=None):
+        self.location = location
+        self.next = next
+        self.prev = prev
+
+    def get_location(self):
+        return self.location
+
+    def get_next(self):
+        return self.next
+
+    def get_prev(self):
+        return self.prev
+
+    def set_location(self, location):
+        self.location = location
+
+    def set_next(self, next):
+        self.next = next
+
+    def set_prev(self, prev):
+        self.prev = prev
+
+
 class Snake:
-    def __init__(self, direction, coordinates, is_growing):
+    def __init__(self, head=None, tail=None, direction, is_growing):
+        self.head = head
+        self.tail = tail
+        self.length = 0
         self.direction = direction
-        self.coordinates = coordinates
         self.is_growing = is_growing
 
-    def get_coordinates(self, coordinates):
-        return self.coordinates
+    def get_head(self):
+        return self.head
+
+    def add_head(self, new_head):
+        if self.head is None:
+            self.tail = new_head
+        else:
+            self.head.set_prev(new_head)
+            new_head.set_next(self.head)
+        self.head = new_head
+        self.length += 1
+
+    def add_last(self, node):
+        if self.tail is None:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+        self.tail = node
+        self.length += 1
+
+    def rem_tail(self):
+        last = self.tail.get_data()
+        self.tail = self.tail.get_prev()
+        if self.tail is None:
+            self.head = None
+        else:
+            self.tail.next.set_prev(None)
+            self.tail.set_next(None)
+        self.length -= 1
+        return last
+
+    def get_coordinates(self):
+        coords = []
+        cur = self.head
+        while cur is not None:
+            coords.append(cur.get_location())
+            cur = cur.next()
+        return coords
+
+    def is_empty(self):
+        return self.head == None
 
     def movement_requirements(self, movekey):
         #todo: do we want to be able to move few steps at a time? return a
@@ -22,7 +89,7 @@ class Snake:
         :param movekey:
         :return:
         """
-        head_location = self.coordinates[0]
+        head_location = self.get_head()
         if movekey == UP and self.direction != DOWN:
             return head_location[0], head_location[1] + 1
         elif movekey == DOWN and self.direction != UP:
@@ -32,7 +99,7 @@ class Snake:
         elif movekey == RIGHT and self.direction != LEFT:
             return head_location[0] + 1, head_location[1]
         else: #unkown move
-            return False
+            return None
 
     def move_snake(self, movekey):
         """
@@ -44,9 +111,9 @@ class Snake:
         movekey that does not match the current direction).
         """
         head_location = self.movement_requirements(movekey)
-        if head_location:
-            self.coordinates[0] = head_location
+        if head_location is not None:
+            self.add_head(Node(head_location))
             if not self.is_growing:  # remove tail
-                self.coordinates.pop(-1)
+                self.rem_tail()
             return True
         return False
